12fb3e43a47a97c79b38880991c8d5361fa38a1c
Christoph Buttler <christoph.buttler@outlook.com>
2018-11-19 17:30:20 +0100

    reworked structure

--- src/sha1.c
+++ src/sha1.c
@@ -17,22 +17,16 @@
 #include "sha1.h"
 
 // constants for hash state initialisation
-#define SHA1_IV_0           UINT32_C(0x67452301)
-#define SHA1_IV_1           UINT32_C(0xEFCDAB89)
-#define SHA1_IV_2           UINT32_C(0x98BADCFE)
-#define SHA1_IV_3           UINT32_C(0x10325476)
-#define SHA1_IV_4           UINT32_C(0xC3D2E1F0)
+#define SHA1_IV_0   UINT32_C(0x67452301)
+#define SHA1_IV_1   UINT32_C(0xEFCDAB89)
+#define SHA1_IV_2   UINT32_C(0x98BADCFE)
+#define SHA1_IV_3   UINT32_C(0x10325476)
+#define SHA1_IV_4   UINT32_C(0xC3D2E1F0)
 // constants for k-values
-#define K_00_19             UINT32_C(0x5A827999)
-#define K_20_39             UINT32_C(0x6ED9EBA1)
-#define K_40_59             UINT32_C(0x8F1BBCDC)
-#define K_60_79             UINT32_C(0xCA62C1D6)
-// constants for initial step optimization
-#define ROUND_CONSTANT_00   UINT32_C(0x9FB498B3)
-#define ROUND_CONSTANT_01   UINT32_C(0x66B0CD0D)
-#define ROUND_CONSTANT_02   UINT32_C(0xF33D5697)
-#define ROUND_CONSTANT_03   UINT32_C(0xD675E47B)
-#define ROUND_CONSTANT_04   UINT32_C(0xB453C259)
+#define K_00_19     UINT32_C(0x5A827999)
+#define K_20_39     UINT32_C(0x6ED9EBA1)
+#define K_40_59     UINT32_C(0x8F1BBCDC)
+#define K_60_79     UINT32_C(0xCA62C1D6)
 
 // macros for f-functions
 #define F_00_19(mB, mC, mD) (mD ^ (mB & (mC ^ mD)))
@@ -44,34 +38,29 @@
 #define GEN_BLOCK(i)                                                                                              \
     temp = (p_blocks[(i - 3) & 15] ^ p_blocks[(i - 8) & 15] ^ p_blocks[(i - 14) & 15] ^ p_blocks[(i - 16) & 15]); \
     p_blocks[i & 15] = LEFT_ROTATE(temp, 1);
-// macros for the round additions in first 5 rounds (adding all constant parts at once)
-#define ROUND_ADDITION_00                 (ROUND_CONSTANT_00 + p_blocks[0])
-#define ROUND_ADDITION_01(mA)             (ROUND_CONSTANT_01 + LEFT_ROTATE(mA, 5) + p_blocks[1])
-#define ROUND_ADDITION_02(mA, mB, mC, mD) (ROUND_CONSTANT_02 + LEFT_ROTATE(mA, 5) + F_00_19(mB, mC, mD) + p_blocks[2])
-#define ROUND_ADDITION_03(mA, mB, mC, mD) (ROUND_CONSTANT_03 + LEFT_ROTATE(mA, 5) + F_00_19(mB, mC, mD) + p_blocks[3])
-#define ROUND_ADDITION_04(mA, mB, mC, mD) (ROUND_CONSTANT_04 + LEFT_ROTATE(mA, 5) + F_00_19(mB, mC, mD) + p_blocks[4])
-// macros for setting state variables in each round
-#define ROUND_PROCESSING_START(mA, mB, mE, mRoundAddition) \
-    mE = mRoundAddition;                                   \
-    mB = LEFT_ROTATE(mB, 30);
-#define ROUND_PROCESSING_REST(mA, mB, mC, mD, mE, f, k, i)     \
+// macro for setting state variables in each round
+#define ROUND_PROCESSING(mA, mB, mC, mD, mE, f, k, i)     \
     mE = k + mE + LEFT_ROTATE(mA, 5) + f + (p_blocks[i & 15]); \
     mB = LEFT_ROTATE(mB, 30);
 // macros for the round functions
-#define ROUND_00_04(mA, mB, mC, mD, mE, mRoundAddition) \
-    ROUND_PROCESSING_START(mA, mB, mE, mRoundAddition)
-#define ROUND_05_19(mA, mB, mC, mD, mE, i) \
+#define ROUND_00_15(mA, mB, mC, mD, mE, i) \
+    ROUND_PROCESSING(mA, mB, mC, mD, mE, F_00_19(mB, mC, mD), K_00_19, i)
+#define ROUND_16_19(mA, mB, mC, mD, mE, i) \
     GEN_BLOCK(i)                           \
-    ROUND_PROCESSING_REST(mA, mB, mC, mD, mE, F_00_19(mB, mC, mD), K_00_19, i)
+    ROUND_PROCESSING(mA, mB, mC, mD, mE, F_00_19(mB, mC, mD), K_00_19, i)
 #define ROUND_20_39(mA, mB, mC, mD, mE, i) \
     GEN_BLOCK(i)                           \
-    ROUND_PROCESSING_REST(mA, mB, mC, mD, mE, F_REST(mB, mC, mD), K_20_39, i)
+    ROUND_PROCESSING(mA, mB, mC, mD, mE, F_REST(mB, mC, mD),  K_20_39, i)
 #define ROUND_40_59(mA, mB, mC, mD, mE, i) \
     GEN_BLOCK(i)                           \
-    ROUND_PROCESSING_REST(mA, mB, mC, mD, mE, F_40_59(mB, mC, mD), K_40_59, i)
+    ROUND_PROCESSING(mA, mB, mC, mD, mE, F_40_59(mB, mC, mD), K_40_59, i)
 #define ROUND_60_79(mA, mB, mC, mD, mE, i) \
     GEN_BLOCK(i)                           \
-    ROUND_PROCESSING_REST(mA, mB, mC, mD, mE, F_REST(mB, mC, mD), K_60_79, i)
+    ROUND_PROCESSING(mA, mB, mC, mD, mE, F_REST(mB, mC, mD),  K_60_79, i)
+
+// function prototypes
+static inline void sha1Pad(char *p_input, uint32_t *p_preprocessedBlocks);
+static void sha1Update(char *p_currInput, struct hash *p_hashState);
 
 /**
  * Function: sha1Digest
@@ -84,46 +73,64 @@ int sha1Digest
     struct hash *p_result
 )
 {
-    uint32_t a, b, c, d, e, temp;
-    uint32_t p_blocks[16];
-    // TODO: split input into blocks and calculate word like below
-    p_blocks[0] = (p_input[0] << 24)
-                | (p_input[1] << 16)
-                | (p_input[2] << 8)
-                | (p_input[3]);
+    int         err = 0;
+    struct hash hashState;
     // initialize state variables with constants
-    a = SHA1_IV_0;
-    b = SHA1_IV_1;
-    c = SHA1_IV_2;
-    d = SHA1_IV_3;
-    e = SHA1_IV_4;
+    hashState.a = SHA1_IV_0;
+    hashState.b = SHA1_IV_1;
+    hashState.c = SHA1_IV_2;
+    hashState.d = SHA1_IV_3;
+    hashState.e = SHA1_IV_4;
+
+    return err;
+}
+/**
+ * Function: sha1Update
+ * 
+ * TODO: add doc here
+ */
+static void sha1Update
+(
+    char        *p_currInput,
+    struct hash *p_hashState
+)
+{
+    static uint32_t a, b, c, d, e, temp;
+           uint32_t p_blocks[16];
+    // TODO: split current input into blocks and calculate word like below
+    p_blocks[0] = (p_currInput[0] << 24)
+                | (p_currInput[1] << 16)
+                | (p_currInput[2] << 8)
+                | (p_currInput[3]);
+    // set state variables to current hash state
+    a = p_hashState->a;
+    b = p_hashState->b;
+    c = p_hashState->c;
+    d = p_hashState->d;
+    e = p_hashState->e;
     /************ UNROLLED ROUND FUNCTION LOOPS ************/
-    // round  00
-    ROUND_00_04(a, b, c, d, e, ROUND_ADDITION_00)
-    // round  01
-    ROUND_00_04(e, a, b, c, d, ROUND_ADDITION_01(e))
-    // round  02
-    ROUND_00_04(d, e, a, b, c, ROUND_ADDITION_02(d, e, a, b))
-    // round  03
-    ROUND_00_04(c, d, e, a, b, ROUND_ADDITION_03(c, d, e, a))
-    // round  04
-    ROUND_00_04(b, c, d, e, a, ROUND_ADDITION_04(b, c, d, e))
-    // rounds 05 - 19
-    ROUND_05_19(a, b, c, d, e,  5)
-    ROUND_05_19(e, a, b, c, d,  6)
-    ROUND_05_19(d, e, a, b, c,  7)
-    ROUND_05_19(c, d, e, a, b,  8)
-    ROUND_05_19(b, c, d, e, a,  9)
-    ROUND_05_19(a, b, c, d, e, 10)
-    ROUND_05_19(e, a, b, c, d, 11)
-    ROUND_05_19(d, e, a, b, c, 12)
-    ROUND_05_19(c, d, e, a, b, 13)
-    ROUND_05_19(b, c, d, e, a, 14)
-    ROUND_05_19(a, b, c, d, e, 15)
-    ROUND_05_19(e, a, b, c, d, 16)
-    ROUND_05_19(d, e, a, b, c, 17)
-    ROUND_05_19(c, d, e, a, b, 18)
-    ROUND_05_19(b, c, d, e, a, 19)
+    // rounds 00 - 15
+    ROUND_00_15(a, b, c, d, e,  0)
+    ROUND_00_15(e, a, b, c, d,  1)
+    ROUND_00_15(d, e, a, b, c,  2)
+    ROUND_00_15(c, d, e, a, b,  3)
+    ROUND_00_15(b, c, d, e, a,  4)
+    ROUND_00_15(a, b, c, d, e,  5)
+    ROUND_00_15(e, a, b, c, d,  6)
+    ROUND_00_15(d, e, a, b, c,  7)
+    ROUND_00_15(c, d, e, a, b,  8)
+    ROUND_00_15(b, c, d, e, a,  9)
+    ROUND_00_15(a, b, c, d, e, 10)
+    ROUND_00_15(e, a, b, c, d, 11)
+    ROUND_00_15(d, e, a, b, c, 12)
+    ROUND_00_15(c, d, e, a, b, 13)
+    ROUND_00_15(b, c, d, e, a, 14)
+    ROUND_00_15(a, b, c, d, e, 15)
+    // rounds 16 - 19
+    ROUND_16_19(e, a, b, c, d, 16)
+    ROUND_16_19(d, e, a, b, c, 17)
+    ROUND_16_19(c, d, e, a, b, 18)
+    ROUND_16_19(b, c, d, e, a, 19)
     // rounds 20 - 39
     ROUND_20_39(a, b, c, d, e, 20)
     ROUND_20_39(e, a, b, c, d, 21)
@@ -188,9 +195,23 @@ int sha1Digest
     ROUND_60_79(c, d, e, a, b, 78)
     ROUND_60_79(b, c, d, e, a, 79)
     /*******************************************************/
-    p_result->a = SHA1_IV_0 + a;
-    p_result->b = SHA1_IV_1 + b;
-    p_result->c = SHA1_IV_2 + c;
-    p_result->d = SHA1_IV_3 + d;
-    p_result->e = SHA1_IV_4 + e;
+    // add this chunks hash to hash state
+    p_hashState->a += a;
+    p_hashState->b += b;
+    p_hashState->c += c;
+    p_hashState->d += d;
+    p_hashState->e += e;
+}
+/**
+ * Function: sha1Pad
+ * 
+ * TODO: add doc here
+ */
+static inline void sha1Pad
+(
+    char     *p_input,
+    uint32_t *p_preprocessedBlocks
+)
+{
+
 }
